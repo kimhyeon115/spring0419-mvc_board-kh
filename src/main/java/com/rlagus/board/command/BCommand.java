3a65b1261ecdae193fdae5948123e67c30f85125
rlagus115 <rlagus115@naver.com>
2023-04-19 17:55:08 +0900

    Spring0419

---
+++ src/main/java/com/rlagus/board/command/BCommand.java
@@ -0,0 +1,9 @@
+package com.rlagus.board.command;
+
+import org.springframework.ui.Model;
+
+public interface BCommand {
+
+	public void execute(Model model);
+	
+}
---
+++ src/main/java/com/rlagus/board/controller/BoardController.java
@@ -0,0 +1,15 @@
+package com.rlagus.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BoardController {
+
+	@RequestMapping(value = "/write_form")
+	public String write() {
+		
+		return "writeForm";
+	}
+	
+}
